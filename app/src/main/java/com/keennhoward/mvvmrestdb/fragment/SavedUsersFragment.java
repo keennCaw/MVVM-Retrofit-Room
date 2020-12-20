eb591c4540b9365f6a308115df60d10ce8358f1e
Kino <keennhc@gmail.com>
2020-12-21 03:28:43 +0800

    added swipe to delete function to savedUsersFragment

--- app/src/main/java/com/keennhoward/mvvmrestdb/fragment/SavedUsersFragment.java
+++ app/src/main/java/com/keennhoward/mvvmrestdb/fragment/SavedUsersFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +17,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.keennhoward.mvvmrestdb.R;
 import com.keennhoward.mvvmrestdb.SavedUsersAdapter;
@@ -51,6 +53,19 @@ public class SavedUsersFragment extends Fragment {
                 savedUsersAdapter.submitList(users);
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                savedUsersViewModel.delete(savedUsersAdapter.getUserAt(viewHolder.getAdapterPosition()));
+                Toast.makeText(getContext(), "User Deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
 
         return v;
     }
